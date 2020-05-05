71d38f1e2b4e5b5153440a3b1cf98967259f7a51
Baidi Wang <baidi.wang@journalism.cuny.edu>
2020-05-05 18:29:47 -0400

    finally works version

--- main.js
+++ main.js
@@ -6,3 +6,25 @@ $("body").on("change", ".filter", function (e) {
 	$(".item.active").removeClass("active");
 	$(`.item.${choice}`).addClass("active");
 });
+
+
+
+$("body").on("click", "a.img", function (e) {
+	e.preventDefault()
+
+	const this_a = e.target;
+	const img_src = $(this_a).attr("src");
+	const caption = $(this_a).data("caption");
+
+	$(".modal img").attr("src", img_src);
+	$(".modal .caption").text(caption);
+
+	$(".modal").addClass("active");
+});
+
+$("body").on("click", ".modal.active, .modal .close", function (e) {
+	e.preventDefault();
+	e.stopPropagation();
+
+	$(".modal.active").removeClass("active");
+});
